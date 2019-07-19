2cc463800e87dfcf29f18b54f59e245113680f51
Kael Shipman <kael.shipman@gmail.com>
2019-07-18 23:30:25 -0500

    Added request-promise-native simple client

---
+++ packages/ts-simple-http-client-rpn/src/index.ts
@@ -0,0 +1,63 @@
+import {
+  SimpleHttpRequestConfig,
+  SimpleHttpResponseInterface,
+  SimpleHttpClientInterface,
+} from "ts-simple-interfaces";
+import * as rpn from "request-promise-native";
+import * as req from "request";
+
+export interface SimpleRpnRequestConfig extends SimpleHttpRequestConfig {
+  transform?: (body: any, response: req.Response, resolveWithFullResponse?: boolean) => any;
+  transform2xxOnly?: boolean;
+  json?: any;
+  removeRefererHeader?: boolean;
+  rejectUnauthorized?: boolean;
+}
+
+export class SimpleHttpClientAxios implements SimpleHttpClientInterface {
+  protected rpn: rpn.RequestPromiseAPI;
+
+  constructor(
+    deps?: { rpn?: rpn.RequestPromiseAPI }
+  ) {
+    if (deps && deps.rpn) {
+      this.rpn = deps.rpn;
+    } else {
+      this.rpn = rpn;
+    }
+  }
+
+  // TODO: Implement handling for throwErrors option
+  // TODO: Implement special data handling for rpn-specific options
+  request<T extends any>(
+    config: SimpleRpnRequestConfig
+  ): Promise<SimpleHttpResponseInterface<T>> {
+    const rpnConfig: rpn.OptionsWithUrl = {
+      baseUrl: config.baseURL,
+      url: config.url || "/",
+      method: config.method,
+      headers: config.headers,
+      transform: config.transform,
+      transform2xxOnly: config.transform2xxOnly,
+      json: config.json,
+      maxRedirects: config.maxRedirects,
+      removeRefererHeader: config.removeRefererHeader,
+      timeout: config.timeout,
+      rejectUnauthorized: config.rejectUnauthorized,
+      followAllRedirects: true,
+      resolveWithFullResponse: true,
+    };
+    config.url = config.url!;
+    return this.rpn(rpnConfig).then(
+      (r: rpn.FullResponse): SimpleHttpResponseInterface<T> => {
+        return {
+          data: r.body,
+          status: r.statusCode,
+          headers: r.headers,
+          config
+        }
+      }
+    );
+  }
+}
+
---
+++ packages/ts-simple-http-client-rpn/tests/e2e.test.ts
@@ -0,0 +1,25 @@
+import "jest";
+import { SimpleHttpClientAxios } from "../src";
+import { SimpleHttpResponseInterface } from "ts-simple-interfaces";
+
+declare interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
+
+test("Can get API response", async () => {
+  const client = new SimpleHttpClientAxios();
+  const r: SimpleHttpResponseInterface = await client.request<Todo>({
+    url: "https://jsonplaceholder.typicode.com/todos/1",
+    json: true
+  });
+
+  expect(r.status).toBe(200);
+  expect(typeof r.data).toBe("object");
+  expect(typeof r.data.id).toBe("number");
+  expect(typeof r.data.userId).toBe("number");
+  expect(typeof r.data.completed).toBe("boolean");
+  expect(typeof r.data.title).toBe("string");
+});
