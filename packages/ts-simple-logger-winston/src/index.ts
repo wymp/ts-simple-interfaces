e471ae2a71bcb724b00e25810a5235e4a455df5a
Kael Shipman <kael.shipman@cfxtrading.com>
2019-08-15 18:27:27 -0500

    Added simple-logger-winston package

---
+++ packages/ts-simple-logger-winston/src/index.ts
@@ -0,0 +1,56 @@
+import { SimpleLoggerInterface } from "ts-simple-interfaces";
+import * as winston from "winston";
+
+export class SimpleLoggerWinston implements SimpleLoggerInterface {
+  protected winston: winston.Logger;
+  public constructor(opts: winston.LoggerOptions = {}) {
+    // force syslog methods
+    opts.levels = winston.config.syslog.levels;
+    this.winston = winston.createLogger(opts);
+  }
+
+  public log(level: string, message: string, ...meta: any[]): this {
+    (this.winston.log as any).apply(this.winston, [ level, message, ...meta ]);
+    return this;
+  }
+
+  public debug(message: string, ...meta: any[]): this {
+    (this.winston.debug as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+
+  public info(message: string, ...meta: any[]): this {
+    (this.winston.info as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+
+  public notice(message: string, ...meta: any[]): this {
+    (this.winston.notice as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+
+  public warning(message: string, ...meta: any[]): this {
+    (this.winston.warning as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+
+  public error(message: string, ...meta: any[]): this {
+    (this.winston.error as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+
+  public alert(message: string, ...meta: any[]): this {
+    (this.winston.alert as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+
+  public critical(message: string, ...meta: any[]): this {
+    (this.winston.crit as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+
+  public emergency(message: string, ...meta: any[]): this {
+    (this.winston.emerg as any).apply(this.winston, [ message, ...meta ]);
+    return this;
+  }
+}
---
+++ packages/ts-simple-logger-winston/tests/SimpleLoggerWinston.spec.ts
@@ -0,0 +1,24 @@
+import { SimpleLoggerWinston } from "../src/";
+import { SimpleLoggerInterface } from "ts-simple-interfaces";
+import * as winston from "winston";
+
+const log: SimpleLoggerInterface = new SimpleLoggerWinston({
+  transports: new winston.transports.Console()
+});
+
+describe("SimpleLoggerWinston", () => {
+  for (const level of [
+    "debug",
+    "info",
+    "notice",
+    "warning",
+    "error",
+    "critical",
+    "alert",
+    "emergency"
+  ] as Array<keyof SimpleLoggerInterface>) {
+    it(`should successfully log ${level} messages`, () => {
+      expect(() => { return (log[level] as any)(`Testing ${level} messages`) }).not.toThrow();
+    });
+  }
+});
