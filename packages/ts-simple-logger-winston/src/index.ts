5343ceb5534d8596cb783cb8069fdcdf519387f0
Kael Shipman <kael.shipman@cfxtrading.com>
2019-08-27 15:13:01 -0500

    Fixed bug in passing meta values through

--- packages/ts-simple-logger-winston/src/index.ts
+++ packages/ts-simple-logger-winston/src/index.ts
@@ -10,47 +10,47 @@ export class SimpleLoggerWinston implements SimpleLoggerInterface {
   }
 
   public log(level: string, message: string, ...meta: any[]): this {
-    (this.winston.log as any).apply(this.winston, [ level, message, ...meta ]);
+    (this.winston.log as any).apply(this.winston, [ level, message ].concat(meta));
     return this;
   }
 
   public debug(message: string, ...meta: any[]): this {
-    (this.winston.debug as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.debug as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 
   public info(message: string, ...meta: any[]): this {
-    (this.winston.info as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.info as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 
   public notice(message: string, ...meta: any[]): this {
-    (this.winston.notice as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.notice as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 
   public warning(message: string, ...meta: any[]): this {
-    (this.winston.warning as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.warning as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 
   public error(message: string, ...meta: any[]): this {
-    (this.winston.error as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.error as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 
   public alert(message: string, ...meta: any[]): this {
-    (this.winston.alert as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.alert as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 
   public critical(message: string, ...meta: any[]): this {
-    (this.winston.crit as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.crit as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 
   public emergency(message: string, ...meta: any[]): this {
-    (this.winston.emerg as any).apply(this.winston, [ message, ...meta ]);
+    (this.winston.emerg as any).apply(this.winston, [ message ].concat(meta));
     return this;
   }
 }
