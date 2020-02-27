b709bf4937f1de4ad3dffc64f2a49310d90d8657
Kael Shipman <kael.shipman@cfxtrading.com>
2020-02-27 14:15:54 -0600

    Updated pubsub interfaces for cleanliness

--- packages/ts-simple-interfaces/src/index.ts
+++ packages/ts-simple-interfaces/src/index.ts
@@ -5,7 +5,7 @@
 /**
  * An interface that presents a method for publishing a message
  *
- * Implementations of this interface are expected to specify a valid type for
+ * Implementations of this interface are expected to specify a concrete type for
  * the msg parameter. This allows implementations to choose their own valid message
  * formats.
  */
@@ -19,30 +19,48 @@ export interface SimplePublisherInterface {
    * all necessary information for your implementation of the publisher to specify
    * the correct routing parameters.
    */
-  publishToChannel: (
+  publish: (
     channel: string,
     routingKey: string,
     msg: unknown,
     options?: unknown
   ) => Promise<void>;
+
+  /**
+   * Close the connection
+   */
+  close: () => Promise<unknown>;
 }
 
 /**
- * An interface that presents a method for subscribing to a message stream with optional
- * routing keys.
+ * An interface that presents a method for subscribing to a message stream.
  *
- * In AMQP paradigm, 'stream' would be the exchange, while routingKey would be something
+ * In AMQP paradigm, `Channel` would be the exchange, while `RoutingKey` would be something
  * like 'app.created.users'.
- *
- * Note that in most cases, implementations of this interface will likely implement
- * some sort of EventEmitter behavior as well.
  */
+declare type RoutingKey = string;
 export interface SimpleSubscriberInterface {
-  subscribeToChannel: (
-    channels: Array<string>,
-    routingKeys?: string[],
+  subscribe: (
+    routes: { [channel: string]: Array<RoutingKey> },
     options?: unknown
   ) => Promise<SimpleSubscriptionInterface>;
+
+  /**
+   * Close the connection
+   */
+  close: () => Promise<unknown>;
+}
+
+/**
+ * An interface describing the simplest type of message. All messages must have at minimum a
+ * `content` field containing a string or buffer with their contents. Implementations of this
+ * interface should resist the temptation to deserialize messages, as it gets very confusing
+ * trying to figure out when and where a message is expected to be in it's final form. That
+ * job should be delegated to higher level libraries.
+ */
+export interface SimpleSubscriptionMessageInterface {
+  content: string | Buffer;
+  extra?: unknown;
 }
 
 /**
@@ -54,11 +72,21 @@ export interface SimpleSubscriberInterface {
 export interface SimpleSubscriptionInterface {
   on: (
     event: "receive" | "disconnect" | "error",
-    listener: (routingKey: string, msg: unknown, options?: unknown) => void
+    listener: (
+      channel: string,
+      routingKey: string,
+      msg: SimpleSubscriptionMessageInterface,
+      options?: unknown
+    ) => void
   ) => SimpleSubscriptionInterface;
   removeListener: (
     event: "receive" | "disconnect" | "error",
-    listener: (routingKey: string, msg: unknown, options?: unknown) => void
+    listener: (
+      channel: string,
+      routingKey: string,
+      msg: SimpleSubscriptionMessageInterface,
+      options?: unknown
+    ) => void
   ) => SimpleSubscriptionInterface;
   removeAllListeners: (event?: "receive" | "disconnect" | "error") => SimpleSubscriptionInterface;
 }
@@ -70,26 +98,64 @@ export interface SimplePubSubInterface extends SimplePublisherInterface, SimpleS
 
 /**
  * An interface that provides a good starting point for a typical Event
+ *
+ * Note that this is intended to be extended in your domain to include whatever other data you
+ * might want in an event. This is not meant to be a complete event object.
  */
-export interface SimpleEvent {
+export interface SimpleSubmittedEvent {
   /** A UUID for the event */
-  id: string;
+  id?: string;
 
   /** The time in MS at which the event occurred */
-  timestamp: number;
+  timestamp?: number;
 
   /** The domain in which the event was produced*/
-  domain: string;
+  domain?: string;
 
   /** The event type (may be coincident with the routing key in AMQP systems) */
   type?: string;
 
-  /** The event ID that caused this event to be produced, if applicable */
-  parentId?: string | null;
+  /** A stack of event ID that caused this event to be produced, if applicable */
+  parentIds?: Array<string> | null;
 
   /** A field for miscellaneous data */
   meta?: unknown;
 }
+export interface SimpleEvent extends SimpleSubmittedEvent {
+  id: string;
+  timestamp: number;
+  domain: string;
+}
+
+/**
+ * An interface that provides a good starting point (based on JsonRpc) for a task-type message
+ */
+export interface SimpleSubmittedTask {
+  /** A UUID for the task */
+  id?: string;
+
+  /** A stack of event ID that caused this event to be produced, if applicable */
+  parents?: Array<string> | null
+
+  /** The domain in which the task was produced*/
+  domain?: string;
+
+  /** The method to be called (see JsonRpc spec) */
+  method: string;
+
+  /** The parameters to be supplied (see JsonRpc spec) */
+  params?: (
+    { [name: string]: string | number | boolean | object | null } |
+    Array<string | number | boolean | object | null>
+  );
+
+  /** Misc extra data */
+  meta?: unknown;
+}
+export interface SimpleTask extends SimpleSubmittedTask {
+  id: string;
+  domain: string;
+};
 
 
 
