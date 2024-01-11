a30c55f4ec935c9bb90764900fc4915d7a1fff27
Kael Shipman <kael.shipman@gmail.com>
2024-01-11 16:49:15 -0700

    minor formatting fixes

--- libs/ts-simple-db-mysql/tests/global-setup.js
+++ libs/ts-simple-db-mysql/tests/global-setup.js
@@ -1,7 +1,8 @@
-const { v2: compose } = require('docker-compose');
+/* eslint-disable */
+const { v2: compose } = require("docker-compose");
 
 module.exports = async () => {
   process.stdout.write(`\n\nStarting docker stack for testing... `);
-  await compose.upAll({ cwd: __dirname, log: false, commandOptions: ['--wait'] });
+  await compose.upAll({ cwd: __dirname, log: false, commandOptions: ["--wait"] });
   process.stdout.write(`done.\n`);
-}
+};
--- libs/ts-simple-db-mysql/tests/global-teardown.js
+++ libs/ts-simple-db-mysql/tests/global-teardown.js
@@ -1,7 +1,8 @@
-const { v2: compose } = require('docker-compose');
+/* eslint-disable */
+const { v2: compose } = require("docker-compose");
 
 module.exports = async () => {
   process.stdout.write(`\nStopping docker stack... `);
   await compose.down({ cwd: __dirname, log: false });
   process.stdout.write(`done.\n\n`);
-}
+};
